0eeb718c3e429602e9242a9b509bf837b720e7ba
taffy99 <452339112@qq.com>
2019-03-23 18:07:48 +0800

    新闻列表接口调用

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,42 +9,73 @@ Page({
     navList:[
       {
         navText:'国内',
+        navcode:"gn",
         isSelected:true
       },
       {
         navText: '国际',
+        navcode: "gj",
         isSelected: false
       },
       {
         
         navText: '财经',
+        navcode: "cj",
         isSelected: false
       },
       {
        
         navText: '娱乐',
+        navcode: "yl",
         isSelected: false
       },
       {
         navText: '军事',
+        navcode: "js",
         isSelected: false
       },
       {
         navText: '体育',
+        navcode: "ty",
         isSelected: false
       },
       {
         navText: '其他',
+        navcode: "other",
         isSelected: false
       }
-    ]
+    ],
+    newsList:[], //新闻列表
+    navSelected:"gn" //选中导航
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getNewsList(this.data.navSelected);
+  },
+  getNewsList(navcode,callBack){
+      wx.request({
+        url: 'https://test-miniprogram.com/api/news/list',
+        data:{
+          type: navcode
+        },
+        success:(res)=>{
+          let newslist = res.data.result;
+          //console.log(newslist);
+          newslist.forEach(item=>{
+            item.date = item.date.replace("T"," ").replace(".000Z","");
+            item.firstImage = "http:"+ item.firstImage;
+          })
+          this.setData({
+            newsList:newslist
+          })
+        },
+        complete:()=>{
+          callBack && callBack();
+        }
+      })
   },
   //导航栏事件
   navSelect(e){
@@ -61,8 +92,17 @@ Page({
       }
     });
     this.setData({
-      navList: navlist
-    })
+      navList: navlist,
+      navSelected:navlist[selectIndex].navcode
+    });
+    this.getNewsList(this.data.navSelected);
+  },
+  //跳转新闻详情页
+  toNewsdetail(e){
+    let id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '../newsDetail/newsDetail?id='+id
+    });
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -96,7 +136,9 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+    this.getNewsList(this.data.navSelected,()=>{
+      wx.stopPullDownRefresh();
+    })
   },
 
   /**
